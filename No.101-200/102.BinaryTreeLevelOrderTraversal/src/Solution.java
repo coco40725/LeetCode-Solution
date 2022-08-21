7e6bf711cf0235bdb4faf5f0b02b31e253befa63
coco40725 <s0912571225@gmail.com>
2022-08-21 15:16:42 +0800

    Leetcode 2022/8/21

--- No.101-200/102.BinaryTreeLevelOrderTraversal/src/Solution.java
+++ No.101-200/102.BinaryTreeLevelOrderTraversal/src/Solution.java
@@ -1,7 +1,22 @@
+import com.sun.source.tree.Tree;
 
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
+/*
+* Stack : Last in first out (LIFO)
+* Queue: First in first out (FIFO)
+*
+* Queue method:
+*  1. add 、 offer (Throws Exception) : Inserts the specified element into this queue if it is possible to do so immediately without violating capacity restrictions.
+*  2. poll、 remove (Throws Exception) : Retrieves and removes the head of this queue.
+*  3. peek、 element (Throws Exception) : Retrieves, but does not remove, the head of this queue.
+*
+* BFS and DFS example: https://magiclen.org/dfs-bfs/
+* BFS, use Queue; https://mycollegenotebook.medium.com/%E5%9C%96%E7%9A%84%E8%B5%B0%E8%A8%AA-bfs-dfs-2-a5dd4b7bd701
+* DFS, use Stack. https://mycollegenotebook.medium.com/%E5%9C%96%E7%9A%84%E8%B5%B0%E8%A8%AA-bfs-dfs-1-8a635ea89013
+*
+* */
+
 import java.util.List;
 
 /**
@@ -9,21 +24,33 @@ import java.util.List;
  * @create 2022-08-19-下午 03:43
  */
 public class Solution {
+    // BFS
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list = new ArrayList<>();
+        // we will use queue to record the all nodes at ith level
+        List<List<Integer>> result = new ArrayList<>();
 
+        if (root == null) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
 
-
-
-        return list;
+        queue.add(root);
+        // while loop: up to down
+        while(!queue.isEmpty()){
+            // for loop: scan the all nodes at ith level
+            // subList will record the values of all nodes at ith level
+            int levelSize = queue.size();
+            List<Integer> subList = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++){
+                TreeNode node = queue.poll();
+                subList.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            result.add(subList);
+        }
+        return result;
 
     }
-    public  List<Integer> collectRootValue(TreeNode root){
-        List<Integer> list = new ArrayList<>();
-        if (root.left != null)   list.add(root.left.val);
-        if (root.right != null) list.add(root.right.val);
-        return list;
-    }
+
 }
 
 class TreeNode {
